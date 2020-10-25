d24393b6148c462af024ed4737e53129ef013efb
^Felipe <felipesqf@gmail.com>
2020-10-25 18:06:35 +1100

    beta version running

--- index.js
+++ index.js
@@ -1,16 +1,91 @@
+var fs = require('fs');
+var inquirer = require("inquirer");
 // array of questions for user
-const questions = [
+const questions = [];
 
-];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Please enter the Project Title",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Enter the Description",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Enter Table of Contents",
+      name: "tableOfContents"
+    },
+    {
+        type: "input",
+        message: "Enter Installation",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "License",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Contributing",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Tests",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Questions",
+        name: "questions"
+    }
+  ])
+  .then(function(response) {
+    const file = `# Title
+    ${response.title}
+## Description
+    ${response.description}
+## Content
+    ${response.tableOfContents}
+## Instalation
+    ${response.installation}
+## Usage
+    ${response.usage}
+## License
+    ${response.license}
+## Contributing
+    ${response.contributing}
+## Tests
+    ${response.tests}
+## Questions
+    ${response.questions};`
+
+
+    fs.writeFile('readmet.md', file, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+
+  })
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+
 
 // function to initialize program
-function init() {
+// function init() {
 
-}
+// };
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
---
+++ readmet.md
@@ -0,0 +1,18 @@
+# Title
+    FElipe Ferreira 
+## Description
+    This is a readme generator
+## Content
+    index,html and css file
+## Instalation
+    Just double click and press ok
+## Usage
+    For those who are tired to create readmefiles
+## License
+    NON
+## Contributing
+    ALI and Sandes
+## Tests
+    NA
+## Questions
+    none;
